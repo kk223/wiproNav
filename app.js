536be7ffa327cd487805513e7be69d834901c7a2
kaushal jha <kaushaljha48@gmail.com>
2017-09-17 13:20:03 -0700

    google strategy added

---
+++ app.js
@@ -0,0 +1,67 @@
+var express = require('express');
+var path = require('path');
+var favicon = require('serve-favicon');
+var logger = require('morgan');
+var cookieParser = require('cookie-parser');
+var bodyParser = require('body-parser');
+
+var index = require('./routes/index');
+var users = require('./routes/users');
+
+var app = express();
+
+// view engine setup
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'jade');
+
+// uncomment after placing your favicon in /public
+//app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
+app.use(logger('dev'));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, 'public')));
+
+
+app.use('/', index);
+app.use('/users', users);
+//
+app.use(function(req, res) {
+    let err = new Error('Resource not found');
+    err.status = 404;
+    return res.status(err.status).json({error: err.message});
+});
+
+app.use(function(err, req, res) {
+    logger.error('Internal error in watch processor: ', err);
+    return res.status(err.status || 500).json({error: err.message});
+});
+
+//  route middleware
+function isLoggedIn(req, res, next) {
+    if (req.isAuthenticated() === true)
+        return next();
+    res.status(201).send('');
+    return 1;
+}
+
+//
+// catch 404 and forward to error handler
+// app.use(function(req, res, next) {
+//   var err = new Error('Not Found');
+//   err.status = 404;
+//   next(err);
+// });
+
+// // error handler
+// app.use(function(err, req, res, next) {
+//   // set locals, only providing error in development
+//   res.locals.message = err.message;
+//   res.locals.error = req.app.get('env') === 'development' ? err : {};
+
+//   // render the error page
+//   res.status(err.status || 500);
+//   res.render('error');
+// });
+
+module.exports = app;
---
+++ config/config.js
@@ -0,0 +1,8 @@
+GOOGLE_AUTH: {
+	clinetID:"client_id":"479564436864-m3dl96psurs8hd577maoppbg7utqdqiq.apps.googleusercontent.com",
+	clientSecret: "iqMrd8WkkZ8ycMTK_qDGg5Bn",
+	callbackURL: '/auth/google/callback'
+},
+JWT_AUTH: {
+        secret: 'wiproNavigator'
+    }  
